47193145070fe025110ddc5df66c19edbe7463f2
Florian <dekontaminator@googlemail.com>
2017-08-18 22:18:43 +0200

    add worker to push event towards redis

---
+++ worker.js
@@ -0,0 +1,44 @@
+var redis = require("redis");
+var request = require('request');
+var publisher  = redis.createClient(process.env.REDIS_URL);
+var subscriber  = redis.createClient(process.env.REDIS_URL);
+
+var  minutely = 60*1*1000;
+var hourly = minutely*60;
+var localhost = "localhost:"+(process.env.PORT || 5000);
+
+	var pollGameday = function() {
+		request("http://"+localhost+"/api/currentGameday",function (error, response, body) {
+			publisher.publish("gameday", body);
+		}).on('error', function(){
+			console.log(error);
+		});
+	};
+
+	setInterval(pollGameday, minutely );
+
+	
+	subscriber.on("message", function(channel, message) {
+		if(!message){
+			return;
+		}
+		var payload = JSON.parse(message);
+		  if(channel==="gameday"){
+			  request("http://"+localhost+"/api/result/"+payload.season+"/"+payload.gameday,function (error, response, body) {
+					payload.results = JSON.parse(body);
+					publisher.publish("results", JSON.stringify(payload));
+				}).on('error', function(){
+					console.log(error);
+				});
+		  };
+		  if(channel==="results"){
+			  	console.log(payload);
+		  };
+	});
+
+	subscriber.subscribe("gameday");
+	subscriber.subscribe("results");
+	
+	
+	console.log('Node worker is runnin');
+
