51a42440abeabf6e828fc94103a56186e2b614b3
florianmarsch <florianmarsch@192.168.0.143>
2017-08-19 15:09:54 +0200

    improve log

--- debug.js
+++ debug.js
@@ -7,7 +7,7 @@ var subscriber  = redis.createClient(process.env.REDIS_URL);
 	
 	subscriber.on("message", function(channel, message) {
 
-		console.log("debug log  "+channel+" : " + message);
+		console.log(new Date() +" debug log  "+channel+" : " + message);
 		
 	});
 
