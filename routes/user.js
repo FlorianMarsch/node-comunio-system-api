4fd18664281386fb71c5521b41e06dd40651677f
Florian Marsch <florianmarsch@Florians-MacBook-Pro.local>
2019-02-27 22:00:34 +0100

    add login

---
+++ routes/user.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var router = express.Router();
+var htmlToJson = require('html-to-json');
+var unorm = require('unorm');
+var request = require('request');
+
+module.exports = function () {
+
+
+	router.post('/login', function (req, res) {
+
+		var j = request.jar();
+		request.post({
+			url: 'https://classic.comunio.de/login.phtml', form: {
+				login: req.body.username,
+				pass: req.body.password,
+				action: 'login',
+				'>> Login_x': 33
+			},
+			jar: j
+		}, function (err, httpResponse, body) {
+
+			request.get({
+				url: 'https://classic.comunio.de/team_news.phtml',
+				jar: j
+			}, function (err, httpResponse, body) {
+
+				htmlToJson.parse(body, {
+					'username': function ($doc) {
+						return $doc.find('#username a').text();
+					},
+					'community': function ($doc) {
+						return $doc.find('#community_name a').text();
+					},
+					'id': function ($doc) {
+						return $doc.find('#userid p').text().replace('ID: DE', '');
+					},
+					'budget': function ($doc) {
+						return $doc.find('#userbudget p').text().replace(/[^a-zA-Z0-9À-ž\s]/g, "").split(" ")[1].trim();
+					},
+					'teamvalue': function ($doc) {
+						return $doc.find('#teamvalue p').text().replace(/[^a-zA-Z0-9À-ž\s]/g, "").split(" ")[1].trim();
+					}
+
+				}, function (err, profile) {
+					request.get({
+						url: 'https://classic.comunio.de/playerInfo.phtml?pid=' + profile.id,
+						jar: j
+					}, function (err, httpResponse, body) {
+
+						htmlToJson.parse(body, {
+							'nickname': function ($doc) {
+								return $doc.find('h1').text().split("(")[0].trim();
+							}
+						}, function (err, result) {
+							profile.nickname = result.nickname;
+							res.status(200).send({
+								jar: j.getCookies('https://classic.comunio.de'),
+								profile: profile
+							});
+						});
+					});
+				});
+			})
+		})
+	});
+
+	return router;
+}
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-module.exports = function(){
+module.exports = function () {
     var express = require('express');
     var cookieParser = require('cookie-parser');
     var bodyParser = require('body-parser');
 
     var app = express();
 
-        
+
     app.set('secretKey', (process.env.SECRET_KEY || 'mySecretKey'));
 
     app.use(bodyParser.json());
@@ -13,7 +13,7 @@ module.exports = function(){
     app.use(cookieParser());
 
 
-        
+
 
 
     app.set('port', (process.env.PORT || 5000));
@@ -21,9 +21,12 @@ module.exports = function(){
     var routes = require('./routes/index')();
     app.use('/', routes);
 
+    var routes = require('./routes/user')();
+    app.use('/user', routes);
 
 
-    app.listen(app.get('port'), function() {
+
+    app.listen(app.get('port'), function () {
         console.log('Node server is running on port', app.get('port'));
     });
 };
